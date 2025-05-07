c8b88c1263636a408e31478fb8b3106cba1db90b
Measum Shah <shahmeasum008@gmail.com>
2025-05-07 19:36:54 +0500

    RedirectURL function done

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-import express, { urlencoded } from "express"
+import express from "express"
 import urlRoute from "./routes/url.router.js";
 import connectToMongoDb from "./connectDb.js";
+
+
 const PORT = 8000;
-
-
-
-// DBConnection
-// connectToMongoDb('mongodb://localhost:27017/short-url').then(()=>{
-//   console.log("Mongodb Connected")
-// }).catch((e)=>{
-//   console.log( `Not connected to DB, error occured: ${e}`)
-// })
-
 const app = express();
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-app.listen(PORT, ()=>{
-  console.log(`Server is running on ${PORT}`);
-})
 
-// routes
-app.use('/url',urlRoute)
+//  Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/url', urlRoute);
+app.use('/:shortId',urlRoute);
+
+// DB Connection + Start Server
+connectToMongoDb('mongodb://localhost:27017/short-url')
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.error(`Not connected to DB, error occurred: ${e}`);
+  });
